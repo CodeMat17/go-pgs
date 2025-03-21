d7c52a8eebec53b2d478b546fb00f4b84d61c7ba
CodeMat17 <codemat.biz@gmail.com>
2025-03-21 22:21:38 +0100

    updated

--- app/programs/page.tsx
+++ app/programs/page.tsx
@@ -53,7 +53,7 @@ export default function ProgramsPage() {
   }
 
   return (
-    <div className='bg-gray-50 dark:bg-gray-900 w-full max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 py-12'>
+    <div className='w-full max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 py-12'>
       <h1 className='text-3xl sm:text-4xl font-bold mb-4'>
         Our Programs
       </h1>
