03847c1293cbead989337dff46a7e7cbbc7f0d8e
CodeMat17 <codemat.biz@gmail.com>
2025-05-02 20:06:56 +0100

    updated

--- convex/schema.ts
+++ convex/schema.ts
@@ -167,9 +167,7 @@ export default defineSchema({
       })
     ),
     faculty: facultyType,
-    courseMaterials: v.optional(
-      v.array(v.object({ title: v.string(), file: v.id("_storage") }))
-    ),
+   
   })
     .index("by_slug", ["slug"])
     .index("by_type", ["type"])
