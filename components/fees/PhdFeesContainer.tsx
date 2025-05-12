8ad4b004b242a97ccce4508650398202d3f6f2b2
CodeMat17 <codemat.biz@gmail.com>
2025-05-12 08:47:04 +0100

    updated

--- components/fees/PhdFeesContainer.tsx
+++ components/fees/PhdFeesContainer.tsx
@@ -4,7 +4,8 @@ import { Minus } from "lucide-react";
 
 const PhdFeesContainer = () => {
   const genFees = useQuery(api.fees.getPhdGeneralFees);
-  const natSciFees = useQuery(api.fees.getPhdNatSciFees);
+    const natSciFees = useQuery(api.fees.getPhdNatSciFees);
+    const eduPlusFees = useQuery(api.fees.getPhdEduFees)
 
   return (
     <div>
@@ -64,15 +65,71 @@ const PhdFeesContainer = () => {
               </div>
             ))}
           </div>
+
+          {/* Faculty of Nat Sciences et el */}
           {natSciFees === undefined ? (
             ""
           ) : (
-            <div className='bg-white dark:bg-gray-900 rounded-xl p-5 shadow-md'>
+            <div className='bg-white dark:bg-gray-900 rounded-xl p-5 shadow-md mb-4'>
               <h1 className='text-muted-foreground font-semibold mb-4'>
                 Faculty of Natural Sciences & Environmental Studies (PhD)
               </h1>
               <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
                 {natSciFees.map((fee) => (
+                  <div key={fee._id} className=''>
+                    <div className='flex items-center justify-between text-sm'>
+                      <h1 className='text-lg font-medium'>
+                        {fee.title}: {fee.amount}
+                        {fee.description && (
+                          <span className='text-muted-foreground text-sm ml-2'>
+                            ({fee.description})
+                          </span>
+                        )}
+                      </h1>
+                    </div>
+
+                    <div className='flex flex-col gap-1 font-medium text-sm'>
+                      {fee.details.map((detail, index) => (
+                        <div
+                          key={`${fee._id}-${index}`}
+                          className='font-medium rounded-xl bg-blue-500/10 dark:bg-gray-800 p-4 mt-3'>
+                          <p>
+                            Bank:{" "}
+                            <span className='text-muted-foreground'>
+                              {detail.bank}
+                            </span>
+                          </p>
+                          <p>
+                            Account No:{" "}
+                            <span className='text-muted-foreground'>
+                              {detail.accountNumber}
+                            </span>
+                          </p>
+                          <p>
+                            Account Name:{" "}
+                            <span className='text-muted-foreground'>
+                              {detail.accountName}
+                            </span>
+                          </p>
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          )}
+
+          {/* Faculty od Education et el */}
+          {eduPlusFees === undefined ? (
+            ""
+          ) : (
+            <div className='bg-white dark:bg-gray-900 rounded-xl p-5 shadow-md'>
+              <h1 className='text-muted-foreground font-semibold mb-4'>
+                Faculty of Education/Arts/Management Sciences (PhD)
+              </h1>
+              <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+                {eduPlusFees.map((fee) => (
                   <div key={fee._id} className=''>
                     <div className='flex items-center justify-between text-sm'>
                       <h1 className='text-lg font-medium'>
